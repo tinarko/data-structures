6af41d7a50cbfabe30320a4d7e062aa3262da2cb
tinarko <tinarchan@gmail.com>
2017-02-09 11:26:36 -0800

    Pass all stack tests for the pseudoclassical pattern except "...return keyword"

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // this = Object.create(Object.prototype);
+  
+  this.s = 0;
+  this.storage = {};
+
+  // return this;
 };
 
+Stack.prototype.size = function () {
+  if (this.s < 0) {
+    return 0;
+  }
+  return this.s;
+};
 
+Stack.prototype.push = function (value) {
+  this.storage[this.s] = value;
+  this.s++;
+};
+
+Stack.prototype.pop = function () {
+  this.s--;
+  var popped = this.storage[this.s];
+  delete this.storage[this.s];
+  return popped;
+};
+
+// var stack = new Stack()
