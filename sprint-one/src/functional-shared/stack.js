08017528eb62e47e3f4f880c58c7f344127c5c01
tinarko <tinarchan@gmail.com>
2017-02-08 16:43:21 -0800

    Pass a few tests in stack shared behavior for the functional-shared pattern

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  extend(someInstance, stackMethods);
+  return someInstance;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var stackMethods = {};
 
+var size = 0;
 
+stackMethods.size = function () {
+  if (size < 0 ) {
+    return 0;
+  }
+  return size;
+};
+
+stackMethods.push = function (value) {
+  size++;
+};
+
+stackMethods.pop = function () {
+  debugger;
+  size--;
+};
