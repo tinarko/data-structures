2f5c1fd85265a243fb77ad46ce502b36abd4e7f4
tinarko <tinarchan@gmail.com>
2017-02-09 16:42:56 -0800

    Pass linkedList tests up to "...return the value of the former..."

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,36 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // this.tail = {};
+    // this.tail.value = value;
+    if (this.tail !== null) {
+      this.tail.next = value;
+    }
+    this.tail = Node(value);
+    this.head = this.head || this.tail;
   };
 
   list.removeHead = function() {
+    //move head pointer to next value
+    var removed = this.head.value;
+    this.head.value = this.head.next;
+    return removed;
   };
 
   list.contains = function(target) {
+    // Check all the properties in the list objects
+    if (this.head.value === target) {
+      return true;
+    } 
+    var checkMiddle = function (node) {
+      if (node === target) {
+        return true;
+      } else {
+        return checkMiddle(node.next);
+      }
+      return false;
+    };
+    return checkMiddle(this.head.next);
   };
 
   return list;
@@ -27,3 +51,5 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// var linkedList = LinkedList();
