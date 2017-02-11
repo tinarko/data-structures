758c713d2a85d008ea505733ed765f33b20f7c79
tinarko <tinarchan@gmail.com>
2017-02-11 15:30:39 -0800

    Pass all hashTable tests and add comments on time complexity

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,29 +7,60 @@ var HashTable = function() {
   //return this;
 };
 
-// Time Complexity: 
+// Time Complexity: O(1)
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-  this._storage.set(index, [k, v]);
+  var bucket = this._storage.get(index) || [];
+
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    // check if the key is already in tuples
+    var currentKey = tuple[0];
+    if (currentKey === k) {
+      var currentValue = tuple[1];
+      tuple[1] = v;
+      return currentValue;
+    }
+  }
+    // if the key is not already in tuples
+  bucket.push([k, v]);
+  this._storage.set(index, bucket);
 };
 
-// Time Complexity: 
+// Time Complexity: O(1)
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var retrieved = this._storage.get(index);
 
-  if (retrieved === undefined) {
+  var bucket = this._storage.get(index) || [];
+
+  if (this._storage.get(index) === undefined) {
     return undefined;
+  } else {
+    for (var i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i];
+      if (tuple[0] === k) {
+        return tuple[1];
+      }
+    }
   }
 
-  return retrieved[1];
 };
 
-// Time Complexity: 
+// Time Complexity: 0(1)
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, undefined);
+
+  var bucket = this._storage.get(index) || [];
+
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      bucket.splice(i, 1);
+    }
+  }
+
+  // this._storage.set(index, undefined);
 
 };
 
