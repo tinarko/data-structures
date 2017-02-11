ba224618d3f73c41469b88fc4f7faa55ad95e66d
tinarko <tinarchan@gmail.com>
2017-02-10 16:37:49 -0800

    Pass all set tests and add comments on time complexity

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,24 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
+// Time Complexity: O(1)
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
+// Time Complexity: O(1)
 setPrototype.contains = function(item) {
+  return this._storage[item] !== undefined;
 };
 
+// Time Complexity: O(1)
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
