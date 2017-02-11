703547f5ec20f5f875fb01f2b94f0bd0abb58b44
tinarko <tinarchan@gmail.com>
2017-02-11 10:09:25 -0800

    Pass hashTable tests up to "should not contain values that were removed"

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,24 +1,37 @@
-
+// pseudoclassical style
 
 var HashTable = function() {
+  //this = Object.create(Object.prototype);
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  //return this;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  this._storage.set(index, [k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var retrieved = this._storage.get(index);
+
+  if (retrieved === undefined) {
+    return undefined;
+  }
+  
+  return retrieved[1];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
+
 };
 
 
-
+// var hashTable = new HashTable();
 /*
  * Complexity: What is the time complexity of the above functions?
  */
